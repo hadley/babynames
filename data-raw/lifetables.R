297b61e3b8cd467bb9c2f3270c952d5c3145c0a8
hadley <h.wickham@gmail.com>
2014-06-29 21:44:51 -0700

    Tidy life tables code

--- data-raw/lifetables.R
+++ data-raw/lifetables.R
@@ -1,23 +1,24 @@
+library(XML)
+library(tidyr)
+library(dplyr)
+
 get_lifetables <- function(year){
   url <- "http://www.ssa.gov/oact/NOTES/as120/LifeTables_Tbl_7_%s.html"
-  tab <- XML::readHTMLTable(sprintf(url, year), which = 2, skip = 2)
+  tab <- readHTMLTable(sprintf(url, year), which = 2, skip = 2)
   nms <- c("x", "qx", "lx", "dx", "Lx", "Tx", "ex", "sex")
   tab_m <- setNames(cbind(tab[-1,1:7], 'M'), nms)
   tab_f <- setNames(cbind(tab[-1,9:15], 'F'), nms)
   dat <- rbind(tab_m, tab_f)
 
-  dat <- subset(dat, x != "")
-  dat[,-8] <- apply(dat[,-8], 2, function(x) {
-    as.numeric(gsub(',', '', x))
-  })
-  dat$year = year
-  return(dat)
+  dat <- filter(dat, x != "")
+  dat[-8] <- lapply(dat[-8], extract_numeric)
+  dat$year <- year
+  dat
 }
 
-years = seq(1900, 2010, by = 10)
-lifetables = plyr::ldply(years, get_lifetables)
+years <- seq(1900, 2010, by = 10)
+lifetables <- tbl_df(rbind_all(lapply(years, get_lifetables)))
 save(lifetables, file = 'data/lifetables.rdata', compress = 'xz')
-
 
 # lifetables_2014 = subset(lifetables, x + year == 2014)
 # all_years = seq(1900, 2010, 1)

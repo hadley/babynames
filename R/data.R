dba320a07cee1463f68ea231f4ed8ce7d7962473
Amelia McNamara <amelia.mcnamara@stat.ucla.edu>
2016-08-11 15:24:35 -0700

    clarify what prop means in babynames

--- R/data.R
+++ R/data.R
@@ -5,7 +5,8 @@
 #'
 #' @format A data frame with five variables: \code{year}, \code{sex},
 #'   \code{name}, \code{n} and \code{prop} (\code{n} divided by total number
-#'   of applicants in that year).
+#'   of applicants in that year, which means proportions are of people of 
+#'   that gender with that name born in that year).
 "babynames"
 
 #' Applicants.

c892216e17ee03d8a8a241457091cea232c081c3
rmcd1024@gmail.com <rmcd1024@gmail.com>
2017-02-14 09:00:16 -0600

    update births to 2014
    updated births.R to use read_csv, skipping rows without data

--- data-raw/births.R
+++ data-raw/births.R
@@ -1,22 +1,23 @@
-library(gdata)
 library(dplyr)
 library(tidyr)
+library(readr)
 
 # 1909 - 2001
 # Source: www.census.gov/statab/hist/02HS0013.xls
-raw <- read.csv('data-raw/02HS0013.csv', skip = 15, stringsAsFactors = FALSE, header = FALSE)
+raw <- read_csv('data-raw/02HS0013.csv', skip = 24, col_names = FALSE,
+                n_max = 93)
 births <- raw %>%
   tbl_df() %>%
-  select(year = V1, births = V2) %>%
-  mutate(births = extract_numeric(births) * 1e3, year = extract_numeric(year)) %>%
+  select(year = X1, births = X2) %>%
+  mutate(births = parse_number(births) * 1e3, year = parse_number(year)) %>%
   filter(!is.na(births), !is.na(year))
 
-# 2002 - 2012 Manually extracted from
-# http://www.cdc.gov/nchs/data/nvsr/nvsr62/nvsr62_09.pdf, page 49
+# 2002 - 2014 Manually extracted from
+# https://www.cdc.gov/nchs/data/nvsr/nvsr64/nvsr64_12.pdf, page 51
 recent <- data.frame(
-  year = 2002:2012,
+  year = 2002:2014,
   births = c(4021726, 4089950, 4112052, 4138349, 4265555, 4316233, 4247694,
-    4130665, 3999386, 3953590, 3952841)
+    4130665, 3999386, 3953590, 3952841, 3932181, 3988076)
 )
 
 births <- births %>% rbind(recent) %>% mutate(year = as.integer(year))

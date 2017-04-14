a2062157501810a6cd9ccf2c68e25350291d45c1
hadley <h.wickham@gmail.com>
2017-04-14 11:28:36 -0500

    Actually save the file #20

--- data-raw/births.R
+++ data-raw/births.R
@@ -5,28 +5,45 @@ library(tidyr)
 # 1909 - 2001
 # Source: www.census.gov/statab/hist/02HS0013.xls
 raw <- read_excel('data-raw/02HS0013.xls',
-  skip = 15,
+  range = "A16:B117",
   col_names = FALSE,
   na = "(NA)"
 )
 births <- raw %>%
-  select(year = X__1, births = X__2) %>%
-  mutate(births = parse_number(births) * 1e3) %>%
-  filter(!is.na(births), !is.na(year))
+  transmute(
+    year = parse_integer(X__1),
+    births = X__2 * 1e3) %>%
+  filter(!is.na(births))
 
 # 2002 - 2012 Manually extracted from
 # http://www.cdc.gov/nchs/data/nvsr/nvsr62/nvsr62_09.pdf, page 50 (Table 21)
-# 2013 - 2014 Manually extracted from
-# http://www.cdc.gov/nchs/data/nvsr/nvsr64/nvsr64_12.pdf, page 51 (Table 21)
-recent <- data.frame(
-  year = 2002:2014,
-  births = c(4021726, 4089950, 4112052, 4138349, 4265555, 4316233, 4247694,
-    4130665, 3999386, 3953590, 3952841, 3932181, 3988076)
+# 2002 - 2015 manually extracted from
+# https://www.cdc.gov/nchs/data/nvsr/nvsr66/nvsr66_01.pdf
+
+recent <- tribble(
+  ~year, ~births,
+  2002,  4021726,
+  2003,  4089950,
+  2004,  4112052,
+  2005,  4138349,
+  2006,  4265555,
+  2007,  4316233,
+  2008,  4247694,
+  2009,  4130665,
+  2010,  3999386,
+  2011,  3953590,
+  2012,  3952841,
+  2013,  3932181,
+  2014,  3988076,
+  2015,  3978497
 )
 
 births <- births %>%
-  rbind(recent) %>%
-  mutate(year = as.integer(year),
-         births = as.integer(births))
+  bind_rows(recent) %>%
+  mutate(
+    year = as.integer(year),
+    births = as.integer(births)
+  )
 
-save(births, file = 'data/births.rdata', compress = 'xz')
+write_csv(births, "births.csv")
+devtools::use_data(births, overwrite = TRUE, compress = 'xz')

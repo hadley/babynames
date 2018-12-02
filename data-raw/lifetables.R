0a1caa55bd8ec390354b3abbec127236ff088080
Russell Pierce <russell.pierce@zapier.com>
2018-12-01 22:14:00 -0600

    readHTMLTable no longer worked So, swapped over to rvest.  Needed a few more tweaks because extract_numeric() has been deprecated and read_html doesn't have the same row skipping logic on ingest.  Then updated to use usethis::use_data().

--- data-raw/lifetables.R
+++ data-raw/lifetables.R
@@ -1,33 +1,24 @@
-library(XML)
+library(xml2)
+library(rvest)
 library(tidyr)
 library(dplyr)
+library(usethis)
 
 get_lifetables <- function(year){
   url <- "http://www.ssa.gov/oact/NOTES/as120/LifeTables_Tbl_7_%s.html"
-  tab <- readHTMLTable(sprintf(url, year), which = 2, skip = 2)
+  tab <- xml2::read_html("http://www.ssa.gov/oact/NOTES/as120/LifeTables_Tbl_7_1900.html") %>% rvest::html_table(fill = TRUE) %>% {.[[2]]}
   nms <- c("x", "qx", "lx", "dx", "Lx", "Tx", "ex", "sex")
   tab_m <- setNames(cbind(tab[-1,1:7], 'M'), nms)
   tab_f <- setNames(cbind(tab[-1,9:15], 'F'), nms)
   dat <- rbind(tab_m, tab_f)
 
   dat <- filter(dat, x != "")
-  dat[-8] <- lapply(dat[-8], extract_numeric)
+  dat[-8] <- lapply(dat[-8], readr::parse_number)
+  dat <- filter(dat, x != year)
   dat$year <- year
   dat
 }
 
-years <- seq(1900, 2010, by = 10)
-lifetables <- tbl_df(rbind_all(lapply(years, get_lifetables)))
-save(lifetables, file = 'data/lifetables.rdata', compress = 'xz')
-
-# lifetables_2014 = subset(lifetables, x + year == 2014)
-# all_years = seq(1900, 2010, 1)
-# lifetable_2014 = plyr::ddply(lifetables_2014, "sex", function(d){
-#   f = splinefun(d$year, d$lx, method = 'fmm')
-#   data.frame(
-#     year = all_years,
-#     lx = f(all_years),
-#     sex = d$sex[1]
-#   )
-# })
-# save(lifetable_2014, file = 'data/lifetable_2014.rdata', compress = 'xz')
+years <- seq(1900, 2017, by = 10)
+lifetables <- tbl_df(bind_rows(lapply(years, get_lifetables)))
+usethis::use_data(lifetables, compress = "xz", overwrite = T)

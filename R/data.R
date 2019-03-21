d7a0276b5bb61a9171dc97b0ef611811519ec068
Dan Buch <dan@rstudio.com>
2019-03-21 15:57:06 -0400

    Replace use of "gender" with "sex"
    to match the API and because sex != gender

--- R/data.R
+++ R/data.R
@@ -9,7 +9,7 @@ NULL
 #' @format A data frame with five variables: \code{year}, \code{sex},
 #'   \code{name}, \code{n} and \code{prop} (\code{n} divided by total number
 #'   of applicants in that year, which means proportions are of people of
-#'   that gender with that name born in that year).
+#'   that sex with that name born in that year).
 "babynames"
 
 #' Applicants.
